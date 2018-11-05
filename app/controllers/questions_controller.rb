6d79b027411b9322b060eabd5693d273ba1bea66
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-05 12:58:22 -0500

    she works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,9 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @query = params[:query]
-    # if
-    # else
-    # end
+    if params[:query] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:query].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+# I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise he will answer I don't care, get dressed and go to work!
